4bc70869662088e9cadd96a7f15320f5a76a5c85
clianor <ejrgus94160@gmail.com>
2020-05-10 12:11:55 +0900

    Updates

---
+++ precache-manifest.a69a9e8b00579448d3cc916abc288645.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c533a8d06e1d2c69deb8470cc8520cf8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "34d5fb73971277e9fcc0",
+    "url": "/portfolio/static/css/main.958870ce.chunk.css"
+  },
+  {
+    "revision": "b0fffd4408bddfdba6b4",
+    "url": "/portfolio/static/js/2.76d346f8.chunk.js"
+  },
+  {
+    "revision": "34d5fb73971277e9fcc0",
+    "url": "/portfolio/static/js/main.0c31156a.chunk.js"
+  },
+  {
+    "revision": "4586ca4cb4bf761d461a",
+    "url": "/portfolio/static/js/runtime-main.d01b02bd.js"
+  },
+  {
+    "revision": "8ceb5805cf1c6e897293b10986a84691",
+    "url": "/portfolio/static/media/homeProfile.8ceb5805.jpg"
+  },
+  {
+    "revision": "89dc18562b07cd926f7fe261a914ad46",
+    "url": "/portfolio/static/media/profile.89dc1856.jpg"
+  }
+]);
